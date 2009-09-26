d3c8d33798da7e5aaf68f820c70caddea0317cc1
pulponair <pulponair@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-26 16:08:40 +0000

    [-BUGFIX]: Removed bogus inclusion statement. Resolves #4742.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nh_ttnews_events/trunk@24836 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_nhttnewsevents_hooks.php
+++ class.tx_nhttnewsevents_hooks.php
@@ -1,7 +1,4 @@
 <?php
-require_once(t3lib_extMgm::extPath('nh_ttnews_events') .
-	'pi1/class.tx_nhttnewsevents_pi1.php');
-
 class tx_nhttnewsevents_hooks {
 	public function processSelectConfHook($pObj, $selectConf) {
 		$selectConf['where'] .= ' AND tt_news.tx_nhttnewsevents_hide_in_list=0';
